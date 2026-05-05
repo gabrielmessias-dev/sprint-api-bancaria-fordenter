b7e23d30789063072421335ae8d53e96985147c8
GABRIEL MESSIAS DO ESPIRITO SANTO VELOSO DA GLORIA <025.016231@universidadesenaicimatec.edu.br>
2026-05-05 11:05:22 -0300

    att

--- Program.cs
+++ Program.cs
@@ -164,4 +164,10 @@ app.MapGet("/", () => "API Bancária rodando 🚀");
 // Controllers
 app.MapControllers();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 app.Run();
